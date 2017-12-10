78fb056303b914e71da2c3b357960b3d5829d544
Mateja Podravac <mateja.podravac@vsmti.hr>
2017-12-10 20:32:04 +0100

    task 1

---
+++ DatabaseService/DatabaseService/Class1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DatabaseService
+{
+    public class Crud
+    {
+       public List <Country> GetAllCountries()
+        {
+            List<Country> lAirTrafficCountries = new List<Country>();
+            String sSqlConnectionString = ConfigurationManager.ConnectionStrings["DbConnection"].ConnectionString;
+            using (DbConnection oConnection = new SqlConnection(sSqlConnectionString))
+            using (DbCommand oCommand = oConnection.CreateCommand())
+            {
+                oCommand.CommandText = "SELECT * FROM AIRTRAFFIC_COUNTRIES";
+                oConnection.Open();
+                using (DbDataReader oReader = oCommand.ExecuteReader())
+                {
+                    while (oReader.Read())
+                    {
+                        lAirTrafficCountries.Add(new Country()
+                        {
+                            nCountryID = (int)oReader["COUNTRY_ID"],
+                            sCountryName = (string)oReader["COUNTRY_NAME"],
+                        });
+                    }
+                }
+            }
+            return lAirTrafficCountries;
+        }
+
+        public void AddCountry(Country oCountry)
+        {
+            String sSqlConnectionString = ConfigurationManager.ConnectionStrings["DbConnection"].ConnectionString;
+            using (DbConnection oConnection = new SqlConnection(sSqlConnectionString))
+            using (DbCommand oCommand = oConnection.CreateCommand())
+            {
+                oCommand.CommandText = "INSERT INTO AIRTRAFFIC_COUNTRIES (COUNTRY_NAME) VALUES('" + oCountry.sCountryName + "');";
+                oConnection.Open();
+                using (DbDataReader oReader = oCommand.ExecuteReader())
+                {
+                }
+            }
+        }
+
+        public void DeleteCountry(Country oCountry)
+        {
+            String sSqlConnectionString = ConfigurationManager.ConnectionStrings["DbConnection"].ConnectionString;
+            using (DbConnection oConnection = new SqlConnection(sSqlConnectionString))
+            using (DbCommand oCommand = oConnection.CreateCommand())
+            {
+                oCommand.CommandText = "DELETE FROM AIRTRAFFIC_COUNTRIES WHERE COUNTRY_ID = " + oCountry.nCountryID;
+                oConnection.Open();
+                using (DbDataReader oReader = oCommand.ExecuteReader())
+                {
+                }
+            }
+        }
+
+        public void UpdateCountry(Country oCountry)
+        {
+            String sSqlConnectionString = ConfigurationManager.ConnectionStrings["DbConnection"].ConnectionString;
+            using (DbConnection oConnection = new SqlConnection(sSqlConnectionString))
+            {
+                using (DbCommand oCommand = oConnection.CreateCommand()) 
+                {
+                    oCommand.CommandText = "UPDATE AIRTRAFFIC_COUNTRIES SET COUNTRY_NAME = '" + oCountry.sCountryName + "' WHERE COUNTRY_ID = " + oCountry.nCountryID;
+                    oConnection.Open();
+                    using (DbDataReader oReader = oCommand.ExecuteReader())
+                    {
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ DatabaseService/DatabaseService/Country.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseService
+{
+    public class Country
+    {
+        public int nCountryID { get; set; }
+        public string sCountryName { get; set; }
+    }
+}
